0c4ab8e4f39ac875b902b9e7dbf94345f77e7e37
Peter Levinsky <pele@easj.dk>
2022-02-28 15:52:26 +0100

    Add project files.

---
+++ ScrumBoard/Pages/UserStories/Index.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScrumBoard.Services;
+using ScrumBoardLib.model;
+
+namespace ScrumBoard.Pages.UserStories
+{
+    public class IndexModel : PageModel
+    {
+        private IUserStoryService _userStoryService;
+
+        
+       
+        public List<UserStory> UserStories { get; private set; }
+
+        public IndexModel(IUserStoryService userStoryService)
+        {
+            _userStoryService = userStoryService;
+        }
+
+        public void OnGet()
+        {
+            UserStories = _userStoryService.GetAll();
+        }
+
+
+        public string ShowDescription(String desc)
+        {
+            return (desc.Length < 25) ? desc : desc.Substring(0, 24) + " ...";
+        }
+
+       
+
+    }
+}
